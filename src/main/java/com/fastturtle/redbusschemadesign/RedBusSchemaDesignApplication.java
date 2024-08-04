e2db7f87ce8fe97c93347c95df93bf8ff8578dcf
divya0319 <divygupta0319@gmail.com>
2024-08-04 12:59:18 +0530

    First commit, with required dependencies added, and project run tested with Swagger url

---
+++ src/main/java/com/fastturtle/redbusschemadesign/RedBusSchemaDesignApplication.java
@@ -0,0 +1,13 @@
+package com.fastturtle.redbusschemadesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBusSchemaDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedBusSchemaDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastturtle/redbusschemadesign/RedBusSchemaDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.redbusschemadesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedBusSchemaDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
