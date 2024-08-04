e679ca11f4f74b71cd7b785aabbc02602acce977
divya0319 <divygupta0319@gmail.com>
2024-08-04 16:17:07 +0530

    Added SpringDoc basic config class, added custom server port, and swagger url, also added redirect controller to redirect to swagger url from root

---
+++ src/main/java/com/fastturtle/redbusschemadesign/configs/SpringDocConfig.java
@@ -0,0 +1,36 @@
+package com.fastturtle.redbusschemadesign.configs;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder().group("Red Bus Schema Design")
+                .packagesToScan("com.fastturtle.redbusschemadesign.controllers")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI busTravelBookingSystemOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Red Bus Schema Design")
+                        .description("A Spring Boot app created with a \"Bus travel booking system database schema design\" " +
+                                "as the core idea for its implementation")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop GitHub Documentation")
+                        .url("https://github.com/springdoc/springdoc-openapi"));
+    }
+}
