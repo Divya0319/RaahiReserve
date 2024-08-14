e8775db39447effd001e9150df391ae388f9b99d
divya0319 <divygupta0319@gmail.com>
2024-08-14 10:54:04 +0530

    Added method to fetch all passengers belonging to a booking in BookingRepository

--- src/main/java/com/fastturtle/redbusschemadesign/repositories/BookingRepository.java
+++ src/main/java/com/fastturtle/redbusschemadesign/repositories/BookingRepository.java
@@ -1,14 +1,19 @@
 package com.fastturtle.redbusschemadesign.repositories;
 
 import com.fastturtle.redbusschemadesign.models.Booking;
+import com.fastturtle.redbusschemadesign.models.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     @Query("SELECT AVG(p.amount) FROM Booking b JOIN b.payment p WHERE b.bookingDate = :date")
     Double findAverageCostOfTicketsOnDate(@Param("date") LocalDate date);
+
+    @Query("SELECT p FROM Booking b JOIN b.passengers p WHERE b = :booking")
+    List<Passenger> findAllPassengersInBooking(@Param("booking") Booking booking);
 }
