b7c52015357dd9d3a0e2029fc6368f0cccb3cc20
Divya Gupta <divygupta0319@gmail.com>
2024-08-16 18:57:00 +0530

    Average cost of tickets on date API is integrated with Thymeleaf template for now, along with its error message displaying webpage

--- src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
+++ src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
@@ -4,12 +4,16 @@ import com.fastturtle.redbusschemadesign.dtos.BookingRequest;
 import com.fastturtle.redbusschemadesign.helpers.DateFormatConverter;
 import com.fastturtle.redbusschemadesign.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/bookings")
 public class BookingController {
     private final BookingService bookingService;
@@ -25,9 +29,22 @@ public class BookingController {
     }
 
     @GetMapping("/averageCostOnDate")
-    public ResponseEntity<?> getAverageCostOfTicketsOnDate(@RequestParam String date) {
-        return bookingService.getAverageCostOfTicketsOnDate(LocalDate.parse
+    public String showAverageCostForm() {
+        return "averageCost";
+    }
+
+    @PostMapping("/averageCostOnDate")
+    public String getAverageCostOfTicketsOnDate(@RequestParam String date, Model model) {
+        ResponseEntity<?> response = bookingService.getAverageCostOfTicketsOnDate(LocalDate.parse
                 (new DateFormatConverter().convertDateFormat(date)));
+
+        if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+            model.addAttribute("errorMessage", ((Map<String, String>) response.getBody()).get("error"));
+            return "error";
+        }
+
+        model.addAttribute("averageCost", response.getBody());
+        return "averageCost";
     }
 
     @GetMapping("/fetchPassengersForBooking")
