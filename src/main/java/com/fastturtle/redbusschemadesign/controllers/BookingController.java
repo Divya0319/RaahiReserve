52b112951a400f48865d3ecad26aae1f31a7309b
Divya Gupta <divygupta0319@gmail.com>
2024-08-17 18:24:49 +0530

    Added todo comments, for task list which I need to complete, related to complete bus booking flow.

--- src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
+++ src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
@@ -67,4 +67,20 @@ public class BookingController {
 
         return "findPassengersForBooking";
     }
+
+    //TODO: create a complete bus booking flow
+    //TODO: 1. person should be given option to select source and destination from dropdown
+    //TODO: 2. Put validation that source and destination should be different
+    //TODO: 3. Give option to choose date(or type date, whichever is feasible)
+    //TODO: 4. Give option to choose type of bus(if bus is available on chosen route)
+    //TODO: 5. Then give option to input passenger details
+    //TODO: 6. Also give option to add more passengers, then depending on it, again load that passenger detail form below.every passenger detail should have dropdown for preference of seat(aisle, window, no preference)
+    //TODO: 7. After its done, button should be there to save passengers(it saves passengers and booking as well)
+    //TODO: 8. Then, Give total payable amount option on next page, depending on seat type chosen
+    //TODO: 9. Then, this page will be payment page(a dummy one), where there should be three radio buttons(loaded from enum created in models), and options will be cash, credit card, wallet and debit card
+    //TODO: 10. Then, there should be three buttons below, mark as pending, mark as completed, or mark as failed, which saves payment accordingly.
+    //TODO: 11. Once it is done, payment status with amount is saved to database.
+    //TODO: 12. Then, from index page, there should be option to mark a passenger as traveled
+    //TODO: 13. Input given will be passenger id, once it is given, those passengers in that booking will be marked as traveled.
+
 }
