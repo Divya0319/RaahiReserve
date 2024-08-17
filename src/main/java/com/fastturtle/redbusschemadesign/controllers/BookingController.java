1b591a2f8bd6d0a3842966e59bb56321415c8e53
divya0319 <divygupta0319@gmail.com>
2024-08-17 13:52:41 +0530

    Added both averagecost and findpassengersforbooking method in same Thymeleaf template file, also added some styling, so that contents of page are at center of the screen, did relevant modifications in BookingController

--- src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
+++ src/main/java/com/fastturtle/redbusschemadesign/controllers/BookingController.java
@@ -2,6 +2,7 @@ package com.fastturtle.redbusschemadesign.controllers;
 
 import com.fastturtle.redbusschemadesign.dtos.BookingRequest;
 import com.fastturtle.redbusschemadesign.helpers.DateFormatConverter;
+import com.fastturtle.redbusschemadesign.models.Passenger;
 import com.fastturtle.redbusschemadesign.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -38,17 +40,29 @@ public class BookingController {
         ResponseEntity<?> response = bookingService.getAverageCostOfTicketsOnDate(LocalDate.parse
                 (new DateFormatConverter().convertDateFormat(date)));
 
-        if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
-            model.addAttribute("errorMessage", ((Map<String, String>) response.getBody()).get("error"));
-            return "error";
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Double averageCost = (Double) response.getBody();
+            model.addAttribute("averageCost", averageCost);
+        } else if(response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+            String errorMessage = ((Map<String, String>)response.getBody()).get("error");
+            model.addAttribute("errorMessageForAverageCost", errorMessage);
         }
 
-        model.addAttribute("averageCost", response.getBody());
         return "averageCost";
     }
 
     @GetMapping("/fetchPassengersForBooking")
-    public ResponseEntity<?> fetchAllPassengersForBooking(@RequestParam int bookingId) {
-        return bookingService.fetchAllPassengersForBooking(bookingId);
+    public String fetchAllPassengersForBooking(@RequestParam int bookingId, Model model) {
+        ResponseEntity<?> response = bookingService.fetchAllPassengersForBooking(bookingId);
+
+        if(response.getStatusCode() == HttpStatus.OK) {
+            List<Passenger> passengers = (List<Passenger>) response.getBody();
+            model.addAttribute("passengers", passengers);
+        } else if(response.getStatusCode() == HttpStatus.NOT_FOUND) {
+            String errorMessage = ((Map<String, String>) response.getBody()).get("error");
+            model.addAttribute("errorMessageForFetchPassenger", errorMessage);
+        }
+
+        return "averageCost";
     }
 }
