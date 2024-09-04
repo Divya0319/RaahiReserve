30731d47fe330978305261ac129217390951ea68
Divya Gupta <divygupta0319@gmail.com>
2024-09-05 02:18:24 +0530

    Refactored code of BusDataInitializer and PaymentDataInitializer classes, for beter maintainability

--- src/main/java/com/fastturtle/redbusschemadesign/schema_initializers/PaymentGatewayDataInitializer.java
+++ src/main/java/com/fastturtle/redbusschemadesign/schema_initializers/PaymentGatewayDataInitializer.java
@@ -13,6 +13,44 @@ import java.util.List;
 @Component
 public class PaymentGatewayDataInitializer {
 
+    // Card Details
+    String[] cardNumbers = {"432111111234", "111123455432", "543211111234", "678943211234"};
+    CardType[] cardTypes = {
+            CardType.DEBIT,
+            CardType.CREDIT,
+            CardType.DEBIT,
+            CardType.CREDIT
+    };
+    Byte[] expiryMonths = {11, 12, 8, 9};
+    Integer[] expiryYears = {2027, 2029, 2026, 2025};
+    String[] cVVs = {"123", "321", "456", "678"};
+
+    // Bank Details
+    String[] bankNames = {
+            "HDFC Bank Ltd",
+            "Axis Bank",
+            "State Bank of India",
+            "ICICI Bank",
+            "Bank of Baroda",
+            "Canara Bank",
+            "Punjab National Bank",
+            "Yes Bank Ltd",
+            "Allahabad Bank",
+            "Gramin Bank"
+    };
+    String[] bankCodes = {
+            "HDFC1234567890123456",
+            "AXIS1234567890123456",
+            "SBI12345678901234567",
+            "ICICI123456789012345",
+            "BOB12345678901234567",
+            "CANARA12345678901234",
+            "PNB12345678901234567",
+            "YES12345678901234567",
+            "ALHBD123456789012345",
+            "GRMN1234567890123456"
+    };
+
     private final CardDetailRepository cardDetailRepository;
     private final BankDetailRepository bankDetailRepository;
     private final UserRepository userRepository;
@@ -30,55 +68,29 @@ public class PaymentGatewayDataInitializer {
     @PostConstruct
     @Transactional
     public void init() {
-        insertPaymentData();
+        createAndSaveCardDetails();
+
+        createAndSaveBankDetails();
+
+        createAndSaveUserWallets();
     }
 
-    private void insertPaymentData() {
-
-        // Card Details
-        String[] cardNumbers = {"432111111234", "111123455432", "543211111234", "678943211234"};
-        CardType[] cardTypes = {CardType.DEBIT, CardType.CREDIT, CardType.DEBIT, CardType.CREDIT};
-        Byte[] expiryMonths = {11, 12, 8, 9};
-        Integer[] expiryYears = {2027, 2029, 2026, 2025};
-        String[] cvvs = {"123", "321", "456", "678"};
-
-        // Bank Details
-        String[] bankNames = {
-                "HDFC Bank Ltd",
-                "Axis Bank",
-                "State Bank of India",
-                "ICICI Bank",
-                "Bank of Baroda",
-                "Canara Bank",
-                "Punjab National Bank",
-                "Yes Bank Ltd",
-                "Allahabad Bank",
-                "Gramin Bank"
-        };
-        String[] bankCodes = {
-                "HDFC1234567890123456",
-                "AXIS1234567890123456",
-                "SBI12345678901234567",
-                "ICICI123456789012345",
-                "BOB12345678901234567",
-                "CANARA12345678901234",
-                "PNB12345678901234567",
-                "YES12345678901234567",
-                "ALHBD123456789012345",
-                "GRMN1234567890123456"
-        };
-
+    private void createAndSaveCardDetails() {
         for(int i = 0; i < cardNumbers.length; i++) {
-            CardDetails cardDetails = new CardDetails(cardNumbers[i], cardTypes[i], expiryMonths[i], expiryYears[i], cvvs[i], true);
+            CardDetails cardDetails = new CardDetails(cardNumbers[i], cardTypes[i], expiryMonths[i], expiryYears[i], cVVs[i], true);
             cardDetailRepository.save(cardDetails);
 
         }
+    }
 
+    private void createAndSaveBankDetails() {
         for(int i = 0; i < bankNames.length; i++) {
             BankDetails bankDetails = new BankDetails(bankNames[i], bankCodes[i]);
             bankDetailRepository.save(bankDetails);
         }
+    }
 
+    private void createAndSaveUserWallets() {
         List<User> users = userRepository.findAll();
 
         for(User user : users) {
@@ -86,6 +98,5 @@ public class PaymentGatewayDataInitializer {
             uw.setUser(user);
             userWalletRepository.save(uw);
         }
-
     }
 }
