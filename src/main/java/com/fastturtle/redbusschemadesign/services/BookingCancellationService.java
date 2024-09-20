e015590c0a7cbc615342b699745472ce0283d9a7
divya0319 <divygupta0319@gmail.com>
2024-09-21 00:19:18 +0530

    Now integrated @Scheduled annotation for task scheduling

--- src/main/java/com/fastturtle/redbusschemadesign/services/BookingCancellationService.java
+++ src/main/java/com/fastturtle/redbusschemadesign/services/BookingCancellationService.java
@@ -1,6 +1,7 @@
 package com.fastturtle.redbusschemadesign.services;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Executors;
@@ -10,13 +11,14 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class BookingCancellationService {
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//
+//    @PostConstruct
+//    public void startBookingCheckTask() {
+//        scheduler.scheduleAtFixedRate(this::checkAndCancelUnpaidBookings, 1, 1, TimeUnit.MINUTES);
+//    }
 
-    @PostConstruct
-    public void startBookingCheckTask() {
-        scheduler.scheduleAtFixedRate(this::checkAndCancelUnpaidBookings, 1, 1, TimeUnit.MINUTES);
-    }
-
+    @Scheduled(initialDelay = 60000, fixedRate = 60000)
     private void checkAndCancelUnpaidBookings() {
         System.out.println("Checking unpaid bookings");
         System.out.println("Booking cancellation done");
