c3f93b72e489a504d29d68fedb6ddda51d7f1b63
Divya Gupta <divygupta0319@gmail.com>
2024-09-21 16:14:10 +0530

    Changed logging from sysout to slf4j

--- src/main/java/com/fastturtle/redbusschemadesign/services/BookingCancellationService.java
+++ src/main/java/com/fastturtle/redbusschemadesign/services/BookingCancellationService.java
@@ -1,15 +1,19 @@
 package com.fastturtle.redbusschemadesign.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingCancellationService {
 
+    private final Logger logger = LoggerFactory.getLogger(BookingCancellationService.class);
+
     @Scheduled(initialDelay = 0, fixedRate = 15000)
     private void checkAndCancelUnpaidBookings() {
-        System.out.println("Checking unpaid bookings");
-        System.out.println("Booking cancellation done");
+        logger.info("Checking unpaid bookings");
+        logger.info("Booking cancellation done");
     }
 
 }
